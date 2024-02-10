0b3854bc4f3f186bc97b7b5d57873e614647f316
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-02-10 06:11:54 -0500

    Add Rake

--- assets/js/postcss.config.js
+++ assets/js/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
-      require('tailwindcss'),
+      require("postcss-import"),
+      require('tailwindcss')("./assets/js/tailwind.config.js"),
       require('autoprefixer'),
       require('cssnano')({ preset: 'default' })
     ]
